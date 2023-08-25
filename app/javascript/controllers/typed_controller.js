8db2d02f9b9c712b83eaf254a9bfcf22b6bb98cc
Olivia Feldman <orwfeldman@gmail.com>
2023-08-25 11:24:24 +0200

    Add JS Typed

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -3,17 +3,63 @@ import Typed from 'typed.js';
 
 // Connects to data-controller="typed"
 export default class extends Controller {
-  static targets = ["animation"];
+  static targets = ["animation", "animation2"];
   connect() {
     console.log("🧞‍♂️");
     this.initializeTyped();
-}
+  }
 
-initializeTyped() {
-  new Typed(this.animationTarget, {
-    strings: ["Book your mysterious lair now"],
-    typeSpeed: 30,
-    loop: true,
-  });
-}
-}
+  initializeTyped() {
+    new Typed(this.animationTarget, {
+      strings: ["Book your mysterious", "lair now"],
+      typeSpeed: 30,
+      loop: true
+    })
+
+      // onComplete: () => {
+    // new Typed(this.animation2Target, {
+    //   strings: ["lair now"],
+    //   typeSpeed: 30,
+    //   loop: true,
+    // })
+      // },
+  };
+};
+
+
+// initializeTyped() {
+//   const typedAnimation1 = new Typed(this.animationTarget, {
+//     strings: ["Book your mysterious"],
+//     typeSpeed: 30,
+//     loop: true,
+//     onComplete: () => {
+//       const typedAnimation2 = new Typed(this.animation2Target, {
+//         strings: ["lair now"],
+//         typeSpeed: 30,
+//         loop: true,
+//       });
+//     },
+//   });
+// }
+// }
+
+
+// initializeTyped() {
+//   const typedAnimation1 = new Typed(this.animationTarget, {
+//     strings: ["Book your mysterious"],
+//     typeSpeed: 30,
+//     loop: true,
+//     onComplete: () => {
+//       this.initializeTyped2(); // Start the second animation after the first one completes
+//     },
+//   });
+// }
+
+// initializeTyped2() {
+//   const typedAnimation2 = new Typed(this.animation2Target, {
+//     strings: ["lair now"],
+//     typeSpeed: 30,
+//     loop: true,
+//   });
+// }
+// }
